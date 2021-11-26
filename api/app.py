306425f38221902a9cf6918155f8e19b9105ed4c
Salar Cheema <salar@Salars-MacBook-Air.local>
2021-11-25 20:40:03 -0600

    add functionality for getting prediction from inputted text

--- api/app.py
+++ api/app.py
@@ -1,21 +1,44 @@
-# when you get model pribabilities, make 0.4-0.6 into NEUTRAL
-# freeze requirements.txt
-# use aws hosting for model if possible
-# make model pipeline if needed
-# finish readme
-# remove old strategy from notebook
+# AWS hosting
+# mobile styling
+# host website on pythonanywhere and netlify
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
 
-with open("clf.pkl", "rb") as f:
-    clf = pickle.load(f)
+with open("pipe.pkl", "rb") as f:
+    pipe = pickle.load(f)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", results={})
+
+@app.route("/predict", methods=["GET", "POST"])
+def make_prediction():
+    text = request.form.get("text-input")
+    if request.method == "POST":
+        conservative_probability, liberal_probability = pipe.predict_proba([text])[0]
+        if conservative_probability >= 0.6:
+            party = "Republican"
+        elif conservative_probability <= 0.4:
+            party = "Democrat"
+        else:
+            party = "Neutral"
+
+        # converts decimal probabilities to percents
+        conservative_percent = str(round(conservative_probability*100, 2)) + "%"
+        liberal_percent = str(round(liberal_probability*100, 2)) + "%"
+
+        results = {
+            "Political Party": party,
+            "Conservative": conservative_percent,
+            "Liberal": liberal_percent
+        }
+
+        return render_template("home.html", results = results)
+    else:
+        return
 
 if __name__ == "__main__":
     app.run()
